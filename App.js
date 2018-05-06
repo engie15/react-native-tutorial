5e64a6a8deedc84d9b62d303cbfabfa874d5601a
engie15 <e.matene15@gmail.com>
2018-05-06 17:50:35 +1200

    added navigation bar

--- App.js
+++ App.js
@@ -1,33 +1,20 @@
 import React, { Component } from 'react';
-import { Provider } from 'react-redux'
-import { AppRegistry, StyleSheet, Text, View } from 'react-native';
-import { Button } from 'react-native-elements';
+import { Provider } from 'react-redux';
+import { StackNavigator } from 'react-navigation';
+import MainScreen from './screens/MainScreen';
 
 import store from './client/store';
 
-export default class App extends React.Component {
+export default class App extends Component {
   render() {
+    const MainNavigator = StackNavigator({
+      Main: { screen: MainScreen }
+    });
+
     return (
       <Provider store={store}>
-        <View style={styles.container}>
-          <Text h1>Kia ora whanau!</Text>
-          <Text>Soon to be Jigglypuff</Text>
-          <Button raised
-            icon={{name: 'cached'}}
-            title="Button!" />
-        </View>
+        <MainNavigator />
       </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
-AppRegistry.registerComponent('App', () => MyApp);
